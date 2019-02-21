3ceebb54d737fed9c3bbfa6b2d25b1933cfa9392
yiyenene <goodbyeboredworld@aol.jp>
2019-02-21 21:03:03 +0900

    modify errors

--- lib/yori/errors/invalid_schema_error.rb
+++ lib/yori/errors/invalid_schema_error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Yori
   module Errors
     class InvalidSchemaError < StandardError; end
--- lib/yori/errors/missing_required_field_error.rb
+++ lib/yori/errors/missing_required_field_error.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'yori/errors/invalid_schema_error'
+
 module Yori
   module Errors
     class MissingRequiredFieldError < InvalidSchemaError; end
